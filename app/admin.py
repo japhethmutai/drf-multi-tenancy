962e8083b63a1aacf0a89cf1a35f8ca0d7a4ccd6
japheth <japhemchirie@gmail.com>
2024-01-18 13:10:27 +0300

    add admin support for the Tenant module

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django_tenants.admin import TenantAdminMixin
+from app.models import Domain, Tenant
 
-# Register your models here.
+# Register domain model
+admin.site.register(Domain)
+
+# Register tenant model
+@admin.register(Tenant)
+class TenantAdmin(TenantAdminMixin, admin.ModelAdmin):
+    list_display = ('name', )
--- multi_tenant/settings.py
+++ multi_tenant/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
+    'app.middleware.TenantMainMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
